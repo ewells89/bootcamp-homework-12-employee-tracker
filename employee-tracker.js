b6f29df09a1a64dd865894eaec5d68468e5323c1
Elyse Wells <cewells89@gmail.com>
2020-12-06 20:27:53 -0500

    began scaffolding inquirer functions

--- employee-tracker.js
+++ employee-tracker.js
@@ -21,7 +21,7 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "potato16",
-  database: "top_songsDB"
+  database: "employee_trackerDB"
 });
 
 // ==============================================================================
@@ -30,6 +30,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log(" ______________________________________________________________________________________________\n|    ______                __                         __  ___                                  |\n|   / ____/___ ___  ____  / /___  __  _____  ___     /  |/  /___ _____  ____ _____ ____  _____ |\n|  / __/ / __ `__ \\/ __ \\/ / __ \\/ / / / _ \\/ _ \\   / /|_/ / __ `/ __ \\/ __ `/ __ `/ _ \\/ ___/ |\n| / /___/ / / / / / /_/ / / /_/ / /_/ /  __/  __/  / /  / / /_/ / / / / /_/ / /_/ /  __/ /     | \n|/_____/_/ /_/ /_/ .___/_/\\____/\\__, /\\___/\\___/  /_/  /_/\\__,_/_/ /_/\\__,_/\\__, /\\___/_/      |\n|               /_/            /____/                                      /____/              |\n|______________________________________________________________________________________________|")
   runTracker();
 });
 
@@ -38,5 +39,94 @@ connection.connect(function(err) {
 // ==============================================================================
 
 function runTracker (){
-  console.log("It works!");
-}
+  console.log("it works!");
+  inquirer
+    .prompt({
+      name:"action",
+      type:"rawlist",
+      message:"What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Roles",
+        "View All Departments",
+        "Add a New Department",
+        "Add a New Role",
+        "Add a New Employee",
+        "Manage Employee Roles"
+      ]
+    })
+    .then(function(answer){
+      switch(answer.action){
+        case "View All Employees":
+          viewEmployees();
+          break;
+        
+        case "View All Roles":
+          viewRoles();
+          break;
+        
+        case "View All Departments":
+          viewDepartments();
+          break;
+
+        case "Add a New Department":
+          addDepartment();
+          break;
+        
+        case "Add a New Role":
+          addRole();
+          break;
+
+        case "Add a New Employee":
+          addEmployee();
+          break;
+
+        case "Manage Employee Roles":
+          manageRoles();
+          break;
+      }
+    });
+};
+
+
+
+function viewEmployees(){
+  connection.query("select * from employee;", function(err,res){
+    console.table(res);
+  });
+  console.log("================================================================================================");
+  runTracker();
+};
+
+
+function viewRoles(){
+  console.log("View all the roles.");
+  runTracker();
+};
+
+function viewDepartments() {
+  console.log("View all the departments.");
+  runTracker();
+};
+
+
+function addDepartment(){
+  console.log("Add a department.");
+  runTracker();
+};
+
+function addRole(){
+  console.log("Add a role.");
+  runTracker();
+};
+
+function addEmployee(){
+  console.log("Add an employee.");
+  runTracker();
+};
+
+
+function manageRoles(){
+  console.log("Manage Roles.");
+  runTracker();
+};
